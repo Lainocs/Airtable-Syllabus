19a41fb61252094a34e90818973d050199501074
Nicolas de Garrigues <ndegarrigues@gmail.com>
2022-12-21 17:46:33 +0100

    :wrench: UPDATE socket params

--- front/src/main.js
+++ front/src/main.js
@@ -2,16 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Airtable from 'airtable'
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client'
 
-// title 
+// title
 document.title = 'Fiche Module'
 
 const app = createApp(App)
 app.use(router)
-app.config.globalProperties.$airtable = new Airtable({ apiKey: process.env.VUE_APP_AIRTABLE_API_KEY }).base(process.env.VUE_APP_AIRTABLE_BASE)
-app.config.globalProperties.$socket = io(process.env.VUE_APP_SOCKET_DSN)
+app.config.globalProperties.$airtable = new Airtable({
+	apiKey: process.env.VUE_APP_AIRTABLE_API_KEY,
+}).base(process.env.VUE_APP_AIRTABLE_BASE)
 
-
+app.config.globalProperties.$socket = io(process.env.VUE_APP_SOCKET_DSN, {
+	secure: true,
+	rejectUnauthorized: false,
+})
 
 app.mount('#app')
